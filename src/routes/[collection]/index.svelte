93185ae0d24d5f111da9805b8b57c5b5c1a70e7e
phi <phi1618jp@gmail.com>
2022-03-31 19:30:25 +0900

    fix

--- src/routes/[collection]/index.svelte
+++ src/routes/[collection]/index.svelte
@@ -1,10 +1,12 @@
 <script context="module">
   export async function load({fetch, params}) {
-    let res = await fetch(`/api/${params.collection}`);
+    let collection = params.collection;
+    let res = await fetch(`/api/${collection}`);
     let {items} = await res.json();
 
     return {
       props: {
+        collection,
         items,
       }
     };
@@ -16,6 +18,7 @@
   import Sidebar from "$lib/components/Sidebar.svelte";
   import admin from "$app/admin.js";
 
+  export let collection = '';
   export let items = [];
 
   let Text = forms.Text;
@@ -25,7 +28,9 @@
 <template lang='pug'>
   div.f
     Sidebar.w300.bg-royalblue.text-white(sections='{admin.sections}')
-    main.container-640.px16.py32
-      +each('items as item')
-        div {item.id}. {item.screen_name}
+    main.w-full
+      div.container-960.px16.py32
+        h1.mb16 {collection}
+        +each('items as item')
+          div {item.id}. {item.screen_name}
 </template>
