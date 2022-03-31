1a06e16d6a0f21fa2f72243ba86dd0ec13e2940d
phi <phi1618jp@gmail.com>
2022-04-01 03:41:02 +0900

    edit page のベースを作成

--- src/routes/[collection]/[id].svelte
+++ src/routes/[collection]/[id].svelte
@@ -1,1 +1,36 @@
-<div>詳細</div>
+<script context="module">
+  import admin from "$demo/admin.js"
+  export async function load({fetch, params}) {
+    let collection = params.collection;
+    let id = params.id;
+    let res = await fetch(`/api/${collection}/${id}`);
+    let {item} = await res.json();
+    let content = admin.contents[params.collection];
+
+    return {
+      props: {
+        collection,
+        item,
+        content,
+      }
+    };
+  }
+</script>
+<script>
+  import { Sidebar } from "$lib";
+  
+  export let item;
+  export let content;
+
+  let select = (e) => {
+    goto(`/${content.path}/${e.detail.item.id}`);
+  };
+</script>
+
+<template lang='pug'>
+  div.f
+    Sidebar.w300.bg-royalblue.text-white(sections='{admin.sections}')
+    main.w-full
+      div.container-960.px16.py32
+        h1.mb16 {content.label} / {item.id}
+</template>
