d547922eaa43c75a1b6015671ff09df3134d25be
phi <phi1618jp@gmail.com>
2022-04-02 00:28:45 +0900

    forms を洗い出して追加

--- src/lib/forms/index.js
+++ src/lib/forms/index.js
@@ -2,10 +2,24 @@
 // contents
 import { default as Text } from './Text.svelte';
 import { default as Textarea } from './Textarea.svelte';
+import { default as Number } from './Number.svelte';
+import { default as Switch } from './Switch.svelte';
+import { default as Date } from './Date.svelte';
+import { default as Image } from './Image.svelte';
+import { default as Array } from './Array.svelte';
+import { default as Object } from './Object.svelte';
+import { default as Button } from './Button.svelte';
 import { default as Section } from './Section.svelte';
 
 export default {
   text: Text,
   textarea: Textarea,
+  number: Number,
+  switch: Switch,
+  date: Date,
+  image: Image,
+  array: Array,
+  object: Object,
+  button: Button,
   section: Section,
 };
