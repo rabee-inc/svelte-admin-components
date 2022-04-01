b777d26bd88626dda01777a79a08d60e6bbe8272
phi <phi1618jp@gmail.com>
2022-04-02 00:20:02 +0900

    textarea を実装

---
+++ src/lib/forms/Textarea.svelte
@@ -0,0 +1,12 @@
+<svelte:options accessors={true}/>
+
+<script>
+  export let schema;
+  export let value = 'hoge';
+</script>
+
+<template lang='pug'>
+  label.block
+    div.fs12.mb4 {schema.label}
+    textarea.border.px8.py4(bind:value)
+</template>
--- src/lib/forms/index.js
+++ src/lib/forms/index.js
@@ -1,9 +1,11 @@
 
 // contents
 import { default as Text } from './Text.svelte';
+import { default as Textarea } from './Textarea.svelte';
 import { default as Section } from './Section.svelte';
 
 export default {
   text: Text,
+  textarea: Textarea,
   section: Section,
 };
