a7e8125b6be6c8ba8edc67be62bf71c90593bc51
phi <phi1618jp@gmail.com>
2022-04-02 12:27:34 +0900

    bugfix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,8 @@
 <script context="module">
 </script>
 <script>
-  import * as forms from "$lib/index.js";
   import Sidebar from "$lib/components/Sidebar.svelte";
   import admin from "$demo/admin.js"
-
-  let Text = forms.Text;
 
 </script>
 
@@ -16,5 +13,4 @@
       div.container-960.px16.py32
         div.f.fclm.fm.mb36
           h1.ts-large.mb4 svelte-admin-components demo
-          Text
 </template>
