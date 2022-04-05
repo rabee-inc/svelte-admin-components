1e3f60e3f1e637ddc02e2b1210fbba590158c77f
kinop <kinoshita.kouki@rabee.jp>
2022-04-05 13:32:09 +0900

    Revert "Number formを作成"
    This reverts commit 22e6c56450b8c53bf024abc9e39b066592507ba1.

--- src/lib/forms/Number.svelte
+++ src/lib/forms/Number.svelte
@@ -1,13 +1,1 @@
-<svelte:options accessors={true}/>
-
-<script>
-  export let schema;
-  export let value = '';
-</script>
-
-<template lang='pug'>
-    label.block
-    +if('schema.label')
-      div.fs12.mb4 {schema.label}
-    input.input.w-full(bind:value, type='number')
-</template>
+<div>Todo Number</div>
