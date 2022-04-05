aac23e37635bdcbfec13008f072dfc2bad95e662
tata-jp <tatamori.77@gmail.com>
2022-04-05 13:47:17 +0900

    if

--- src/lib/forms/Date.svelte
+++ src/lib/forms/Date.svelte
@@ -1,17 +1,14 @@
 <svelte:options accessors={true}/>
 
 <script>
-  import { forms } from "$lib/index.js";
-
   export let schema;
-  export let value;
-  console.log(schema,'schema')
-  console.log(value,'value')
+  export let value = '';
 
 </script>
 
 <template lang="pug">
   label.block
-    div.fs12.mb4 {schema.label}
+    +if('schema.label')
+      div.fs12.mb4 {schema.label}
     input.input(bind:value, type='date')
 </template>
