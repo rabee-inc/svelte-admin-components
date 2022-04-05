e7f39edfdbeacb2907cce9b7cfd5436233794b62
tata-jp <tatamori.77@gmail.com>
2022-04-05 13:42:36 +0900

    日付表示してsubmit確認

--- src/lib/forms/Date.svelte
+++ src/lib/forms/Date.svelte
@@ -1,1 +1,17 @@
-<div>Todo Date</div>
+<svelte:options accessors={true}/>
+
+<script>
+  import { forms } from "$lib/index.js";
+
+  export let schema;
+  export let value;
+  console.log(schema,'schema')
+  console.log(value,'value')
+
+</script>
+
+<template lang="pug">
+  label.block
+    div.fs12.mb4 {schema.label}
+    input.input(bind:value, type='date')
+</template>
