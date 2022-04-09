dc70e7a683958e663d63fb148648851c28449eeb
phi <phi1618jp@gmail.com>
2022-04-09 15:28:22 +0900

    再帰的にフォームを設定できるよう対応

--- src/routes/[collection]/edit.svelte
+++ src/routes/[collection]/edit.svelte
@@ -18,7 +18,94 @@
   export let content;
   export let collection;
 
-  let schemas = [
+  const FORM_SCHEMA = [
+    { "key": "key", "label": "key", "type": "text", "class": "col4", },
+    { "key": "label", "label": "label", "type": "text", "class": "col4", },
+    { "key": "type", "label": "type", "type": "text", "class": "col4", },
+    // for textarea
+    {
+      "key": "opts",
+      "label": "opts",
+      "type": "object",
+      "condition": {
+        "key": 'type',
+        "operation": "==",
+        "value": 'textarea',
+      },
+      "opts": {
+        "schemas": [
+          { "key": "cols", "label": "cols", "type": "number", "class": "col4", },
+        ]
+      }
+    },
+    // for number
+    {
+      "key": "opts",
+      "label": "opts",
+      "type": "object",
+      "condition": {
+        "key": 'type',
+        "operation": "==",
+        "value": 'number',
+      },
+      "opts": {
+        "schemas": [
+          { "key": "min", "label": "min", "type": "number", "class": "col4", },
+          { "key": "max", "label": "max", "type": "number", "class": "col4", },
+          { "key": "step", "label": "step", "type": "number", "class": "col4", },
+        ]
+      }
+    },
+    // for array
+    {
+      "key": "opts",
+      "label": "opts",
+      "type": "object",
+      "condition": {
+        "key": 'type',
+        "operation": "==",
+        "value": 'array',
+      },
+      "opts": {
+        "schemas": [
+          { "key": "schema.type", "label": "schema type", "type": "text", "class": "col4", },
+        ]
+      }
+    },
+    // for object
+    {
+      "key": "opts",
+      "label": "opts",
+      "type": "object",
+      "condition": {
+        "key": 'type',
+        "operation": "==",
+        "value": 'object',
+      },
+      "opts": {
+        "schemas": [
+          {
+            "key": "schemas",
+            "label": "schemas",
+            "type": "array",
+            "opts": {
+              "schema": {
+                "type": "object",
+                "opts": () => {
+                  return {
+                    "schemas": FORM_SCHEMA,
+                  };
+                },
+              }
+            }
+          },
+        ]
+      }
+    }
+    // TODO: 他の type 用のも作っていく
+  ];
+
+  const CONTENT_SCHEMA = [
     {
       "key": "label",
       "label": "label",
@@ -34,95 +121,7 @@
         "schema": {
           "type": "object",
           "opts": {
-            "schemas": [
-              { "key": "key", "label": "key", "type": "text", "class": "col4", },
-              { "key": "label", "label": "label", "type": "text", "class": "col4", },
-              { "key": "type", "label": "type", "type": "text", "class": "col4", },
-              // for textarea
-              {
-                "key": "opts",
-                "label": "opts",
-                "type": "object",
-                "condition": {
-                  "key": 'type',
-                  "operation": "==",
-                  "value": 'textarea',
-                },
-                "opts": {
-                  "schemas": [
-                    { "key": "cols", "label": "cols", "type": "number", "class": "col4", },
-                  ]
-                }
-              },
-              // for number
-              {
-                "key": "opts",
-                "label": "opts",
-                "type": "object",
-                "condition": {
-                  "key": 'type',
-                  "operation": "==",
-                  "value": 'number',
-                },
-                "opts": {
-                  "schemas": [
-                    { "key": "min", "label": "min", "type": "number", "class": "col4", },
-                    { "key": "max", "label": "max", "type": "number", "class": "col4", },
-                    { "key": "step", "label": "step", "type": "number", "class": "col4", },
-                  ]
-                }
-              },
-              // for array
-              {
-                "key": "opts",
-                "label": "opts",
-                "type": "object",
-                "condition": {
-                  "key": 'type',
-                  "operation": "==",
-                  "value": 'array',
-                },
-                "opts": {
-                  "schemas": [
-                    { "key": "schema.type", "label": "schema type", "type": "text", "class": "col4", },
-                  ]
-                }
-              },
-              // for object
-              {
-                "key": "opts",
-                "label": "opts",
-                "type": "object",
-                "condition": {
-                  "key": 'type',
-                  "operation": "==",
-                  "value": 'object',
-                },
-                "opts": {
-                  "schemas": [
-                    {
-                      "key": "schemas",
-                      "label": "schemas",
-                      "type": "array",
-                      "opts": {
-                        "schema": {
-                          "type": "object",
-                          "opts": {
-                            "schemas": [
-                              { "key": "key", "label": "key", "type": "text", "class": "col4", },
-                              { "key": "label", "label": "label", "type": "text", "class": "col4", },
-                              { "key": "type", "label": "type", "type": "text", "class": "col4", },
-                            ]
-                          }
-                        }
-                      }
-                    },
-                  ]
-                }
-              }
-
-              // TODO: 他の type 用のも作っていく
-            ],
+            "schemas": FORM_SCHEMA
           },
         }
       }
@@ -153,5 +152,5 @@
     main.w-full
       div.container-960.px16.py32
         h1.mb16 {content.label} edit
-        ContentForm(value='{content}', schemas='{schemas}', actions='{admin.actions}', on:submit='{submit}')
+        ContentForm(value='{content}', schemas='{CONTENT_SCHEMA}', actions='{admin.actions}', on:submit='{submit}')
 </template>
