a859741a6862b155b5ea727e6fa344f0a911c7e9
phi <phi1618jp@gmail.com>
2022-06-17 17:58:37 +0900

    Section コンポーネントに section を渡すよう対応(labelは削除)

--- src/lib/components/ContentForm.svelte
+++ src/lib/components/ContentForm.svelte
@@ -115,6 +115,6 @@
           +each('sections as section,i')
             +if('shouldShowSection(section)')
               div.align-self-top.px8.mb16(class!='{section.class || ""}')
-                svelte:component(this='{sectionComponent}', label='{section.label}')
+                svelte:component(this='{sectionComponent}', section='{section}')
                   svelte:component(bind:this='{instances[i]}', this='{forms.object}', schema='{sectionToObjectSchema(section)}', actions='{actions}', value='{value}', formValue='{formValue}', frame='{false}', on:change='{onChange}')
 </template>
