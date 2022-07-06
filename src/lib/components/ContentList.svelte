0591187207377db8fdaa9c55ac77a81ab731ba3a
tata-jp <tatamori.77@gmail.com>
2022-07-06 12:23:30 +0900

    contentlist に scroll と min-width 指定

--- src/lib/components/ContentList.svelte
+++ src/lib/components/ContentList.svelte
@@ -21,12 +21,12 @@
 </script>
 
 <template lang='pug'>
-  div(class='{className}')
-    table.w-full.w-full.border-spacing-0.border-collapse-collapse
+  div.w-full.overflow-scroll(class='{className}')
+    table.w-full.border-spacing-0.border-collapse-collapse
       thead
         tr.border-bottom.text-left
           +each('headings as heading')
-            th.px12.py16.text-gray.bold.word-break-keep {heading.label}
+            th.min-width-100.px12.py16.text-gray.bold.word-break-keep {heading.label}
       tbody
         +each('items as item')
           tr.border-bottom.transition.hover-bg.cursor-pointer(on:click!='{(e) => select(e, item)}')
