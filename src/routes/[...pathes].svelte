0ded95e00f7fffc4171ed42c6e860d593205c518
phi <phi1618jp@gmail.com>
2022-08-02 22:25:44 +0900

    多段階層パス対応

---
+++ src/routes/[...pathes].svelte
@@ -0,0 +1,39 @@
+<script context="module">
+import { contents } from "$lib";
+
+
+  export async function load({fetch, params}) {
+    let pathes = params.pathes.split('/');
+    let mode = (pathes.length % 2 === 0) ? 'edit' : 'list';
+
+    let content_id;
+
+    if (mode === 'edit') {
+      content_id = pathes[pathes.length  - 2];
+    }
+    else {
+      content_id = pathes[pathes.length  - 1];
+    }
+
+    console.log(content_id);
+    if (!contents.schemas[content_id]) {
+      // return {
+      //   redirect: 404,
+      // }
+    }
+
+    return {
+      props: {
+        mode,
+      }
+    };
+  };
+</script>
+
+<script>
+  export let mode;
+</script>
+
+<template lang="pug">
+  div {mode}
+</template>
