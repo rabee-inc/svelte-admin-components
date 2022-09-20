1fb3afb9c6a7e1e0006dd86338d503d43b1e726d
phi <phi1618jp@gmail.com>
2022-09-20 18:49:15 +0900

    label ではなく path から content を引っ張ってきてパンくずを表示するよう対応

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,11 +6,45 @@
   export let buttons = [];
 
   let className = null;
+  let breadcrumbs = [];
+
+  $: {
+    breadcrumbs = [];
+
+    let pathes = path.split('/');
+    let temp_pathes = [];
+
+    pathes.forEach((p, i) => {
+      temp_pathes.push(p);
+
+      let label = p;
+      let temp_path = temp_pathes.join('/');
+
+      if (i%2 === 0) {
+        let content = actions.pathToContent(temp_path);
+
+        if (content) {
+          label = content.label;
+        }
+      }
+
+      breadcrumbs.push({
+        label,
+        link: `/${temp_path}`,
+      });
+    });
+  }
 </script>
 
 <template lang="pug">
   div.f.fm.flex-between(class='{className}')
-    h1.fs16 {label}
+    div.overflow-scroll
+      h1.fs14.f
+        +each('breadcrumbs as breadcrumb,i')
+          a.flex-fixed(href='{breadcrumb.link}') {breadcrumb.label} 
+          +if('i < (breadcrumbs.length-1)')
+            span.mx4 / 
+
     div.f.fm
       div.f.fm
       +each('buttons as button')
