b2b595121e622492c0dc8dad6817cb7f586069cf
mtnn <ohno.motoki@rabee.jp>
2023-02-22 14:47:31 +0900

    fix getimgixurl

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -36,14 +36,16 @@ export function pathToContent(contents, path) {
 };
 
 // imgix画像の取得
-export function getImgixUrl(image, width = null, height = null) {
+export function getImgixUrl(image, width=null, height=null, fit='crop') {
+  // 既にimgix_urlの場合には return
+  // if (!/imgix\.net/.test(image)) return image;
   let dpr = (typeof devicePixelRatio === 'number') ? devicePixelRatio : 1;
-  let url = `${image}?auto=format&cs=srgb&fit=crop&dpr=${dpr}`;
-  if (width) {
-    url += `&w=${width}`;
-  }
-  if (height) {
-    url += `&h=${height}`;
-  }
-  return url;
+  let url_obj = new URL(image);
+  let query_array = Object.entries({ auto: 'format', cs: 'srgb', fit, dpr, width, height });
+
+  query_array.forEach(([key, value]) => {
+    if (!value) return ;
+    url_obj.searchParams.set(key, String(value));
+  });
+  return url_obj.href;
 };
