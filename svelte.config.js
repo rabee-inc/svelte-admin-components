9b35dba81cc639f20017bf443cf53c536db37acc
suico-jp <ryoorer@gmail.com>
2023-04-18 19:57:40 +0900

    update: svelte.config.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
 import sveltePreprocess from 'svelte-preprocess';
+import path from 'path';
+
 const preprocess = sveltePreprocess({
   typescript: true,
   // ...
 });
-import path from 'path';
 
 let adapter = null;
 
@@ -18,28 +19,16 @@ else {
   adapter = adapterNode();
 }
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter,
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE']
-    },
-
-    vite: {
-      mode: process.env.NODE_ENV,
-      define: {
-        'process.env': process.env,
-      },
-      resolve: {
-        alias: {
-          "svelte-admin-components": path.resolve('./src/lib'),
-          $components: path.resolve('./src/components'),
-          $admin: path.resolve('./src/admin'),
-        }
-      }
+    alias: {
+      '$components': path.resolve('./src/components'),
+      "$admin": path.resolve('./src/admin'),
+      "svelte-admin-components": path.resolve('./src/lib'),
+      // '@rabee-org/svelte-admin-components': path.resolve('./src/lib'),
     }
   },
   preprocess,
