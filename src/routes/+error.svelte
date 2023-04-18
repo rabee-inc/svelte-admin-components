1823428952e32c476ce83c9e563613c489f1efa6
suico-jp <ryoorer@gmail.com>
2023-04-18 20:19:54 +0900

    update: error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,24 +1,13 @@
-<script context="module">
-  /** @type {import('@sveltejs/kit').ErrorLoad} */
-  export function load({ error, status }) {
-    return {
-      props: {
-        status,
-        message: error.message,
-        title: `${status}: ${error.message}`
-      }
-    };
-  }
-</script>
-
 <script>
-  export let status;
-  export let message;
+  import { page } from '$app/stores';
+  let status = $page.status;
+  let message = $page.error.message;
 </script>
 
 <template lang="pug">
   main.f.fh.s-full
     div.py64.text-center
       h1.fs64 {status}
-      p {message}
+      p.mb12 {message}
+      a.button.button.normal(href='/') TOPへ戻る
 </template>
