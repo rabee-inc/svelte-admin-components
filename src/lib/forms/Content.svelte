87c092de9a93298589b29d3ae8f8421397cdb978
kouta-ibuka <ibuka.kouta@rabee.jp>
2024-10-02 13:52:17 +0900

    fix: ArrayでContentの要素を並び替えたときに要素が消えてしまうのを修正

--- src/lib/forms/Content.svelte
+++ src/lib/forms/Content.svelte
@@ -35,16 +35,12 @@
 
     modal.$on('select', (e) => {
       contentItem = e.detail.item;
-
+      value = getByPath(contentItem, schema.opts.value_key);
       dispatch('change');
       modal.close();
     });
   };
 
-  // svelte-ignore unused-export-let
-  export let getValue = () => {
-    return contentItem ? getByPath(contentItem, schema.opts.value_key) : null;
-  };
 </script>
 
 <template lang='pug'>
@@ -60,7 +56,7 @@
           div.fs10.text-danger ※{schema.opts.caution}
     div.f.fm
       +if('contentItem')
-        div.mr16 {getByPath(contentItem, schema.opts.label_key)}
+        div.col8.mr16 {getByPath(contentItem, schema.opts.label_key)}
       div
         button.button(type='button', on:click='{openContentModal}') 選択する
 </template>
