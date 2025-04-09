2262d7c05b6b4f5de4d959e287b7ee62c7804f0d
phi <phi1618jp@gmail.com>
2025-04-10 01:42:56 +0900

    refactor

--- src/lib/forms/Number.svelte
+++ src/lib/forms/Number.svelte
@@ -2,17 +2,13 @@
 
 <script>
   export let schema;
-  export let value = '';
+  export let value = schema.opts?.defaultValue || '';
   // svelte-ignore unused-export-let
   export let formValue;
   // svelte-ignore unused-export-let
   export let actions;
   // svelte-ignore unused-export-let
   export let item;
-
-  if (value === '' && schema.opts?.defaultValue !== undefined) {
-    value = schema.opts?.defaultValue;
-  }
 </script>
 
 <template lang='pug'>
